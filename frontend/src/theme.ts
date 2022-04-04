e5d915875db4afc64b014815860c20dc27e52231
Andrew Tran <tandrew468@gmail.com>
2022-04-04 22:26:29 +1000

    Modified Themes

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -1,6 +1,15 @@
 import { createTheme } from '@mui/material/styles';
 
 // TODO Create a custom theme to use application wide.
-const theme = createTheme();
-
+// https://colorhunt.co/palettes/space  ???
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#03045E',
+    },
+    secondary: {
+      main: '#00B4D8',
+    },
+  },
+});
 export default theme;
