cdd77a49b880cc63c047214312f69113591f574c
Andrew Tran <tandrew468@gmail.com>
2022-04-04 22:25:36 +1000

    Refactored Header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,42 +1,78 @@
-import { Component, ReactNode } from 'react';
-import { AppBar, Button, Toolbar } from '@mui/material';
-import logo from '../../assets/logo.png';
-import { Link } from 'react-router-dom';
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+import logo from '../../assets/RMIT-LOGO-WHITE.png';
 
-class Header extends Component<{ className?: string }, never> {
-  render() {
-    // Elements to render inside header
-    // TODO: update sign-in button according to authentication status
-    const headerElements: ReactNode = (
-      <div className='row flex-grow-1'>
-        <div className='ms-auto col-1'>
-          <Button variant='contained' className='col'>
-            Sign In
-          </Button>
-        </div>
-      </div>
-    );
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
 
-    return (
-      <AppBar
-        sx={{
-          backgroundColor: 'secondary.main',
-          boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.2)',
-        }}
-        position='sticky'
-        className={this.props.className}
-      >
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+export default function SearchAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
         <Toolbar>
-          <div className='col-1'>
-            <Link to='/'>
-              <img src={logo} className='img-fluid' alt='RMIT Logo' />
-            </Link>
-          </div>
-          {headerElements}
+          <Typography variant='h6' noWrap component='div' sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}>
+            <Box sx={{ fontWeight: 'bold' }}>
+              <a href={'/home'}>
+                <img src={logo} alt='RMIT LOGO' height={'40vmin'} />
+              </a>
+            </Box>
+          </Typography>
+          <Typography variant='h6' noWrap component='div' sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+            Microgravity Database
+          </Typography>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder='Search…' inputProps={{ 'aria-label': 'search' }} />
+          </Search>
         </Toolbar>
       </AppBar>
-    );
-  }
+    </Box>
+  );
 }
-
-export default Header;
