b311dd8e949c1eb43f013d022600f01e8bb921d7
Andrew <tandrew468@gmail.com>
2022-04-28 11:19:23 +1000

    Updated Layout of Mission Results

--- frontend/src/components/Results/Mission/MissionResult.tsx
+++ frontend/src/components/Results/Mission/MissionResult.tsx
@@ -1,20 +1,34 @@
 import { Grid, GridProps, Link, Typography } from '@mui/material';
+import { MissionResultsProps } from '../../../util/types';
 
-export default function MissionResult(
-  props: GridProps & {
-    id: string | undefined;
-    name: string | undefined;
-    startDate: string | undefined;
-    endDate: string | undefined;
-  },
-) {
+export default function MissionResult(props: GridProps & MissionResultsProps) {
   const url = `/mission/${props.id}`;
   return (
     <Grid container item component={Link} href={url} bgcolor={props.bgcolor} padding={3} alignItems='baseline'>
       <Grid container item direction='column' md={3} marginRight={5}>
         <Grid item>
-          <Typography color='primary.dark' variant='body1'>
-            {props.name}
+          <Typography color='primary.dark' variant='body1' display={'inline'}>
+            <Typography display={'inline'}> Mission Name : </Typography> {props.name}
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid container item md direction={'row'}>
+        <Grid item m={1}>
+          <Typography display={'inline'}>Start Date : </Typography>
+          <Typography display={'inline'} color={'primary.dark'} variant={'body1'}>
+            {props.startDate}
+          </Typography>
+        </Grid>
+        <Grid item m={1}>
+          <Typography display={'inline'}>End Date : </Typography>
+          <Typography display={'inline'} color={'primary.dark'} variant={'body1'}>
+            {props.endDate}
+          </Typography>
+        </Grid>
+        <Grid item m={1}>
+          <Typography display={'inline'}>Launch Date : </Typography>
+          <Typography display={'inline'} color={'primary.dark'} variant={'body1'}>
+            {props.launchDate}
           </Typography>
         </Grid>
       </Grid>
