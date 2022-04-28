23c8ebb82499b124710b2336f8a11189bc30b1d8
Andrew <tandrew468@gmail.com>
2022-04-28 17:12:20 +1000

    Updated api call for get experiment by id

--- frontend/src/util/api.ts
+++ frontend/src/util/api.ts
@@ -1,8 +1,20 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import { Experiment } from './types';
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 const api = axios.create({
   baseURL: backendUrl,
+});
+
+const makeTypedAPICall =
+  <In, Out>(apiCall: (args: In) => Promise<AxiosResponse<Out>>) =>
+  (args: In) =>
+    apiCall(args).then((res) => res.data);
+
+export const getExperiment = makeTypedAPICall<{ id: string }, Experiment>((args) => {
+  const search = new URLSearchParams();
+  search.append('id', args.id);
+  return api.get(`${backendUrl}/experiments/get?${search.toString()}`);
 });
 
 export default api;
